cc701d71f8523570e52a434ae73861e10a944261
DESKTOP-ES1M2KF\okada <aiokada@hgc.jp>
2017-06-02 10:13:00 +0900

    update

--- check_bam.py
+++ check_bam.py
@@ -21,7 +21,7 @@ check_singlebam.py {path to working dir} {TCGA summary file} {path to bam dir} -
 rev = " $Rev: 120 $"
 
 from multiprocessing import Process
-import pandas
+import json
 import time
 import datetime
 import sys
@@ -48,14 +48,15 @@ export PYTHONHOME=/usr/local/package/python/current2.7
 export PATH=${PYTHONHOME}/bin:${PATH}
 export LD_LIBRARY_PATH=${PYTHONHOME}/lib:${LD_LIBRARY_PATH}
 
-python ./mdtest.py {bam} {out_checksum}
+md5_check=`python ./mdtest.py {bam} {md5}`
 
 total_l=`{samtools} view {bam} | wc -l`
 single_l=`{samtools} view -F 1 {bam} | wc -l`
 
-echo {analysis_id} > {out_single}
-echo `expr $total_l` >> {out_single}
-echo `expr $single_l` >> {out_single}
+echo {analysis_id} > {out}
+echo `expr $md5_check` >> {out}
+echo `expr $total_l` >> {out}
+echo `expr $single_l` >> {out}
 """
 
 def write_log(path, mode, text, date, printer):
@@ -74,7 +75,7 @@ def write_log(path, mode, text, date, printer):
         print (t + text)
 
 
-def qsub_process(name, output_dir, bam, analysis_id, config):
+def qsub_process(name, output_dir, bam, analysis_id, md5, config):
     
     script_path = output_dir + "/scripts/" + name + ".sh"
     log_path = output_dir + "/log/" + name + ".log"
@@ -91,14 +92,13 @@ def qsub_process(name, output_dir, bam, analysis_id, config):
         return 0
 
     cmd = cmd_format.format(samtools = config.get('TOOLS', 'samtools'), 
-                bam = bam, analysis_id = analysis_id, \
+                bam = bam, analysis_id = analysis_id, md5=md5,
                 out = result_path, log = output_dir + "/log")
 
     f_sh = open(script_path, "w")
     f_sh.write(cmd)
     f_sh.close()
     os.chmod(script_path, 0750)
-
 
     s = drmaa.Session()
 
@@ -157,14 +157,17 @@ def main():
     parser = argparse.ArgumentParser(prog = name)
     
     parser.add_argument("--version", action = "version", version = name + rev)
-    parser.add_argument('output_dir', help = "output root directory", type = str)
-    parser.add_argument('summary', help = "summary file download from TCGA", type = str)
+    
+    parser.add_argument('metadata', help = "metdata-file(.json) downloaded from TCGA", type = str)
     parser.add_argument('bam_dir', help = "bam downloaded directory", type = str)
+    parser.add_argument('project', help = "project name (ACC, BLCA, etc)", type = str)
+    
+    parser.add_argument('output_dir', help = "output root directory", type = str)
     parser.add_argument("--config_file", help = "config file", type = str, default = "")
     args = parser.parse_args()
 
     output_dir = os.path.abspath(args.output_dir)
-    summary = os.path.abspath(args.summary)
+    metadata = os.path.abspath(args.metadata)
     bam_dir = os.path.abspath(args.bam_dir)
     
     if len(args.config_file) > 0:
@@ -186,72 +189,67 @@ def main():
     if (os.path.exists(output_dir + "/scripts") == False):
         os.makedirs(output_dir + "/scripts")
     
-    summary_name = os.path.splitext(os.path.basename(summary))[0]
+    project = args.project
     now = datetime.datetime.now()
-    this_name = "check_bam_%s_%04d%02d%02d_%02d%02d%02d" % (summary_name, now.year, now.month, now.day, now.hour, now.minute, now.second)
+    this_name = "check_bam_%s_%04d%02d%02d_%02d%02d%02d" % (project, now.year, now.month, now.day, now.hour, now.minute, now.second)
     log_path = output_dir + "/log/%s.log" % this_name
     write_log(log_path, "w", "Start main process.", True, True)
 
     process_list = []
     
-    # read summary
-    ext = os.path.splitext(summary)[1]
-    if ext.lower() == ".tsv":
-        data = pandas.read_csv(summary, sep = "\t")
-    elif ext.lower() == ".csv":
-        data = pandas.read_csv(summary, sep = ",")
-    else:
-        print (ext + " file is not support")
-        return
-    
-    if len(data) < 1:
-        print ("no summary data.")
-        return
-
+    # read metadata
+    read_data = json.load(open(metadata))
+    data = []
+    for obj in read_data:
+        if not obj["experimental_strategy"] in config.getstr('METADATA', 'experimental_strategy').split(","):
+            continue
+        if not obj["platform"] in config.getstr('METADATA', 'platform').split(","):
+            continue
+        data.push(obj)
+        
     # loop for job start
-    else:        
-        max_once_jobs = config.getint('JOB_CONTROL', 'max_once_jobs')
-        max_all_jobs = max_once_jobs * 2
-        interval = config.getint('JOB_CONTROL', 'interval')
+    max_once_jobs = config.getint('JOB_CONTROL', 'max_once_jobs')
+    max_all_jobs = max_once_jobs * 2
+    interval = config.getint('JOB_CONTROL', 'interval')
+    
+    j = 0
+    while j < len(data):
+        alives = 0
+        for process in process_list:
+            if process.exitcode == None:
+               alives += 1
+    
+        jobs = max_all_jobs - alives
+        if jobs > max_once_jobs:
+            jobs = max_once_jobs
         
-        j = 0
-        while j < len(data):
-            alives = 0
-            for process in process_list:
-                if process.exitcode == None:
-                   alives += 1
+        for i in range(jobs):
+            if j >= len(data):
+                break
+                
+            now = datetime.datetime.now()
+            name = "{name}_{index:0>5d}_{year:0>4d}{month:0>2d}{day:0>2d}_{hour:0>2d}{minute:0>2d}{second:0>2d}".format(
+                    name = project, index = j+1,
+                    year = now.year, month = now.month, day = now.day, 
+                    hour = now.hour, minute = now.minute, second = now.second)
+
+            bam = bam_dir + "/" + data[j]["analysis"]["analysis_id"] + "/" + data[j]["filename"]
+            
+            process = Process(target=qsub_process, name=name, args=(name, output_dir, bam, data[j]["analysis"]["analysis_id"], data[j]["md5sum"], config))
+            process.daemon == True
+            process.start()
+            
+            write_log(log_path, "a", name + ": Start sub process.", True, False)
+
+            process_list.append(process)
+            j += 1
         
-            jobs = max_all_jobs - alives
-            if jobs > max_once_jobs:
-                jobs = max_once_jobs
-            
-            for i in range(jobs):
-                if j >= len(data):
-                    break
-                    
-                now = datetime.datetime.now()
-                name = "{name}_{index:0>5d}_{year:0>4d}{month:0>2d}{day:0>2d}_{hour:0>2d}{minute:0>2d}{second:0>2d}".format(
-                        name = summary_name, index = j+1,
-                        year = now.year, month = now.month, day = now.day, 
-                        hour = now.hour, minute = now.minute, second = now.second)
-
-                bam = bam_dir + "/" + data.iloc[j]["analysis_id"] + "/" + data.iloc[j]["filename"]
-                
-                process = Process(target=qsub_process, name=name, args=(name, output_dir, bam, data.iloc[j]["analysis_id"], config))
-                process.daemon == True
-                process.start()
-                
-                write_log(log_path, "a", name + ": Start sub process.", True, False)
-
-                process_list.append(process)
-                j += 1
-            
-            time.sleep(interval)
+        time.sleep(interval)
 
     # summarize logs and results
     result_path = output_dir + "/result/result_%s.csv" % this_name
     f = open(result_path, "w")
-    f.write("analysis_id,total_lines,single_lines\n")
+    f.write("analysis_id,md5_check,total_lines,single_lines\n")
     f.close()
     
     for process in process_list:
@@ -268,11 +266,11 @@ def main():
         pres = f_result.read()
         f_result.close()
         lines = pres.split("\n")
-        if len(lines) >= 3:
+        if len(lines) >= 4:
             os.remove(output_dir + '/result/' + process.name + ".txt")
             
             f = open(result_path, "a")
-            f.write(lines[0] + "," + lines[1] + "," + lines[2] + "\n")
+            f.write(lines[0] + "," + lines[1] + "," + lines[2] + "," + lines[3] + "\n")
             f.close()
 
     write_log(log_path, "a", "End main process.", True, True)
--- mdtest.py
+++ mdtest.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import hashlib
-
 def get_md5(filename, size = 8192):
+    import hashlib
+
     md5 = hashlib.md5()
     with open(filename, 'rb') as f:
         for chunk in iter(lambda: f.read(size * md5.block_size), b''):
@@ -16,25 +16,13 @@ def main():
     prog = "testmd"
     parser = argparse.ArgumentParser(prog = prog)
     parser.add_argument("--version", action = "version", version = prog + "-0.1.0")
-    parser.add_argument('bam_pattern', help = "bam files to  glob", type = str)
-    parser.add_argument('output', help = "output file", type = str)
+    parser.add_argument('bamfile', help = "bam files to  glob", type = str)
+    parser.add_argument('md5', help = "bam files to  glob", type = str)
+    
     args = parser.parse_args()
 
-    bams = glob.glob(args.bam_pattern)
-    f = open(args.output, "wb")
-    f.write("bam_pattern=%s, output=%s, files=%d\n" % (args.bam_pattern, args.output, len(bams)))
-    f.close()
-
-    for bam in bams:
-        hexdigest = get_md5(bam)
-        f = open(bam + ".md5", 'rb')
-        reference = f.read()
-        f.close()
-
-        with open(args.output, 'ab') as f:
-            f.write("file=%s, ref=%s, copy=%s\n" % (bam, reference, hexdigest))
-            if reference != hexdigest:
-                f.write("[ERROR] MD5 check error! %s\n" % (bam))
+    hexdigest = get_md5(bam);
+    return (hexdigest == args.md5)
 
 if __name__ == "__main__":
     main()
